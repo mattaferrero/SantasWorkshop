b80563e2cd2a692a3ff91cb38b917b987dc265a8
Matt Ferrero <mattaferrero@gmail.com>
2021-12-19 11:30:03 -0500

    To do: grab file data, fix mmap args

--- toymaker.c
+++ toymaker.c
@@ -10,23 +10,51 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
+
+#include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
 
-int main(int argc, char *argv[]) {
+#define MAX_ARGS 2
 
-    char ch;
-    
-    while ( (ch = getopt(argc, argv, "l:i:")) != EOF) {
-        switch(ch) {
-            case 'l': return 0; break;
-            case 'i': return 0; break;
-            default: fprintf(stderr, "Incorrect argument.\n");
-        }
+int main(int argc, char *argv[]) {  
+
+    int fd = 0;
+    int maplen = 0;
+    void *mptr = NULL;
+
+    /* To do: implement args/flags later, do more specific error checking */
+    if (argc != MAX_ARGS) {
+        fprintf(stderr, "Error: exceeded maximum number of arguments. Please man toymaker. Terminating.\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    /* 
+     * mmap() requires a file descriptor, so we are using open() instead for convenience instead of fopen(). 
+     * I _think_ a successful return of fopen() means the file does exist. docs were _not_ helpful.
+     */
+    fd = open(argv[1], O_RDWR); /* O_RDWR read/write. other flags are read only/write only. */
+    if (fd == -1) {
+        fprintf(stderr, "Could not open file, terminating.\n");
+        return 1;
+    }
+
+    /* 
+     * We will load the file into memory with mmap(). Since mmap() does some behind the scenes magic and
+     * doesn't actually load the ENTIRE file until we try accessing those parts of it, we don't care about
+     * file size and can just do a sanity check for the ELF header contents before mucking around.
+     */
+    mptr = mmap(NULL, maplen, PROT_EXEC, MAP_PRIVATE, fd, 0); /* <-- this needs to be fixed stat. grab file data first. */
+    if (mptr == MAP_FAILED) {
+        fprintf(stderr, "Mapping failed. Terminating.\n");
+        return 1;
+    }
+
+    if (close(fd) == -1) {
+        fprintf(stderr, "Could not close file descriptor. Terminating.\n");
+        return 1;
     }
 return 0;
 }
-
-
-
